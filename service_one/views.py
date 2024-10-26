5642b2d1ae47f08c0e47d472a074527405c0f465
Manoj <manu042k@gmail.com>
2024-10-26 13:07:03 -0700

    Fix unused import in views.py

--- service_one/views.py
+++ service_one/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets # type: ignore
+from rest_framework import viewsets # type: ignore 0
 from .models import RandomModel
 from .serializers import RandomModelSerializer
 from rest_framework.permissions import AllowAny # type: ignore
